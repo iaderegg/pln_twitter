fc546365f565371aed9aa291d1c843ccf1b4216e
IaderEGG <iaderegg@laohu-dev.laohu.org>
2017-10-27 03:07:12 -0500

    Algo del backend

--- pln_twitter_morfologico/twitter_morfologico/static/js/main.js
+++ pln_twitter_morfologico/twitter_morfologico/static/js/main.js
@@ -7,6 +7,7 @@ $( document ).ready(function() {
 
     $('#btn_clean').on('click', function(){
         $('#textarea_tweet').val(' ');
+        $('#div_result').html(' ');
     });
 
 });
@@ -24,7 +25,41 @@ function send_tweet(tweet_msg){
         },
         dataType: 'json',
         success: function (data) {
-          console.log(data);
+            console.log(data);
+            var html = "";
+            
+            for(i = 0; i < data.result_morfo.length; i++){
+                
+                word_morfo = data.result_morfo[i].split(" ")
+
+                html += '<div class=\'col-sm-3\'>';
+                html += '<table class=\'table table-striped table-bordered\'>';
+                html += '<thead>';
+                html += '<tr>';
+                html += '<th class=\'col-sm-6\'>Palabra</th>';
+                html += '<th class=\'col-sm-6\'>'+word_morfo[i]+'</th>';
+                html += '</tr>';
+                html += '</thead>';
+                html += '<tbody>';
+
+                for(j = 1; j < word_morfo.length; j++){
+                    html += '<tr>';
+                    html += '<th class=\'col-sm-6\'></th>';
+                    html += '<th class=\'col-sm-6\'>'+word_morfo[j]+'</th>';
+                    html += '</tr>';
+                }
+                    
+
+                html += '</tbody>';
+                html += '</table>';
+                html += '</div>';
+            }            
+
+            $('#div_result').html(html);
+        },
+        error: function(data){
+            console.log(data);
         }
       });
 }
+
--- pln_twitter_morfologico/twitter_morfologico/views.py
+++ pln_twitter_morfologico/twitter_morfologico/views.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
+from django.utils.encoding import python_2_unicode_compatible
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.http import JsonResponse
+from nltk.tokenize import TweetTokenizer
+
+import commands
 
 def index(request):
     template = loader.get_template('twitter_morfologico/index.html')
@@ -15,8 +19,19 @@ def index(request):
 
 def tweet_processing(request):
     tweet = request.POST.get('tweet_msg')
+
+    tknzr = TweetTokenizer()
+
+    result = tknzr.tokenize(tweet)
+    result_morfo = []
+
+    for w in result:
+        print('echo "'+ w +'" | analyze \\xe2\\x80\\x93 analyze -f /usr/share/freeling/config/es.cfg')
+        result_morfo.append(commands.getoutput('echo \''+ w +'\' | analyze \\xe2\\x80\\x93 analyze -f /usr/share/freeling/config/es.cfg'))
+
     data = {
-        'result': 'Si retorna algo',
-        'tweet_msg': tweet
+        'result': result,
+        'tweet_msg': tweet,
+        'result_morfo': result_morfo
     }
     return JsonResponse(data)
