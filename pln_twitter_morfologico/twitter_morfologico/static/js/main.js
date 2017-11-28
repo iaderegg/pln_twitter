c95cc24229b2b89aca36512a0b7fa1c09f77b57c
iaderegg <iadergg@gmail.com>
2017-11-27 21:22:00 -0500

    Version 27 de noviembre

--- pln_twitter_morfologico/twitter_morfologico/static/js/main.js
+++ pln_twitter_morfologico/twitter_morfologico/static/js/main.js
@@ -10,7 +10,9 @@ $( document ).ready(function() {
 
     $('#btn_clean').on('click', function(){
         $('#textarea_tweet').val(' ');
-        $('#div_result').html(' ');
+        $('#textarea_result_tknzr').val(' ');
+        $('#textarea_result_steps_freeling').html(' ');
+        $('#div_result_explain').html(' ');
     });
 
 });
@@ -60,25 +62,92 @@ function processing_tweet(result_tknzr){
     var token = $("input[name=csrfmiddlewaretoken]").val()
 
     var promises = new Array()
+    var result_morfo = new Array()
 
-    for(var j = 0; j < result_tknzr.length; j++){
+    //$('#textarea_result_steps_freeling').val(' ')
+
+    var promise = new Promise(function(resolve, reject){
+
+        for(var j = 0; j < result_tknzr.length; j++){
+
+            if(result_tknzr[j].charAt(0) == '#'){
+
+                print_text('Hashtag: '+result_tknzr[j]+'\n')
+
+            }else{
+                $.ajax({
+                    url: 'word_processing/',
+                    type: 'POST',
+                    data:{
+                        'word': result_tknzr[j],
+                        'csrfmiddlewaretoken': token
+                    },
+                    dataType: 'json',
+                    success: function(result){
+                        print_text(result.word_analysis)
+                        result_morfo.push(result.word_analysis)
+                    },
+                    async: false
+                })
+            }      
+        }
+        resolve(result_morfo)
+    }).then(console.log('Exec promise'))
+
+    
+    //el for debe ser una promesa 
+    explain_morfo_analysis(result_morfo)
+}
+
+
+function explain_morfo_analysis(result_morfo){
+
+    var html = "";
+    
+    var array_morfo_structure = new Array("Lema", "Tag", "Probabilidad");
+    
+    for(i = 0; i < result_morfo.length; i++){
         
-        promises.push($.ajax({
-            url: 'word_processing/',
-            type: 'POST',
-            data:{
-                'word': result_tknzr[j],
-                'csrfmiddlewaretoken': token
-            },
-            dataType: 'json',
-            success: function(result){
-                print_text(result.word_analysis)
-            },
-            async: true
-        }))
-    }
+        word_morfo = result_morfo[i].split(" ")
+        
+        var tag_translate = trans_tag(word_morfo[2]);
 
-    //Promise.all(promises)
+        html += '<div class=\'col-sm-3\'>';
+        html += '<table class=\'table table-striped table-bordered\'>';
+        html += '<thead>';
+        html += '<tr>';
+        html += '<th class=\'col-sm-6\'>Palabra</th>';
+        html += '<th class=\'col-sm-6\'>'+word_morfo[0]+'</th>';
+        html += '</tr>';
+        html += '</thead>';
+        html += '<tbody>';
+
+        for(j = 1; j < word_morfo.length; j++){
+            html += '<tr>';
+            html += '<th class=\'col-sm-6\'>'+array_morfo_structure[j-1]+'</th>';
+            html += '<th class=\'col-sm-6\'>'+word_morfo[j]+'</th>';
+            html += '</tr>';
+        }
+        
+        for(k = 0; k < tag_translate.length; k++){
+            html += '<tr>';
+            html += '<th class=\'col-sm-6\'>'+tag_translate[k].attr+'</th>';
+            html += '<th class=\'col-sm-6\'>'+tag_translate[k].value+'</th>';
+            html += '</tr>';
+
+        }
+
+        html += '</tbody>';
+        html += '</table>';
+        html += '</div>';
+        
+        console.log(word_morfo[2])
+        
+    }            
+
+    $('#div_result_explain').html(html);
+    
+
 }
 
 function print_text(text){
@@ -130,7 +199,7 @@ function send_tweet(tweet_msg){
                 html += '</div>';
                 
                 console.log(word_morfo[2])
-                trans_tag(word_morfo[2]);
+                
             }            
 
             $('#div_result').html(html);
@@ -144,19 +213,21 @@ function send_tweet(tweet_msg){
 }
 
 function trans_tag(tag){
-    console.log(tag);
+    
     var result = new Array();
 
     eagle_tags = {
         A : {
             1: {
                 atributo: 'Categoria',
-                A: 'Adjetivo'
+                A: 'Adjetivo',
+                0: '-'
             },
             2: {
                 atributo: 'Tipo',
                 Q: 'Calificativo',
-                O: 'Ordinal'
+                O: 'Ordinal',
+                0: '-'
             },
             3: {
                 atributo: 'Grado',
@@ -190,18 +261,21 @@ function trans_tag(tag){
         R:{
             1:{
                 atributo: 'Categoria',
-                R: 'Adverbio'
+                R: 'Adverbio',
+                0: '-'
             },
             2:{
                 atributo: 'Tipo',
                 G: 'General',
-                N: 'Negativo'
+                N: 'Negativo',
+                0: '-'
             }
         },
         D:{
             1:{
                 atributo: 'Categoria',
-                D: 'Determinante'
+                D: 'Determinante',
+                0: '-'
             },
             2:{
                 atributo: 'Tipo',
@@ -210,78 +284,91 @@ function trans_tag(tag){
                 T: 'Interrogativo',
                 E: 'Exclamativo',
                 I: 'Indefinido',
-                A: 'Artículo'
+                A: 'Artículo',
+                0: '-'
             },
             3:{
                 atributo: 'Persona',
                 1: 'Primera',
                 2: 'Segunda',
-                3: 'Tercera'
+                3: 'Tercera',
+                0: '-'
             },
             4:{
                 atributo: 'Género',
                 M: 'Masculino',
                 F: 'Femenino',
                 C: 'Común',
-                N: 'Neutro'
+                N: 'Neutro',
+                0: '-'
             },
             5:{
                 atributo: 'Número',
                 S: 'Singular',
                 P: 'Plural',
-                N: 'Invariable'
+                N: 'Invariable',
+                0: '-'
             },
             6:{
                 atributo: 'Poseedor',
                 S: 'Singular',
-                P: 'Plural'
+                P: 'Plural',
+                0: '-'
             }
         },
         N:{
             1:{
                 atributo: 'Categoria',
-                N: 'Nombre'
+                N: 'Nombre',
+                0: '-'
             },
             2:{
                 atributo: 'Tipo',
                 C: 'Común',
-                P: 'Propio'
+                P: 'Propio',
+                0: '-'
             },
             3:{
                 atributo: 'Género',
                 M: 'Masculino',
                 F: 'Femenino',
-                C: 'Común'
+                C: 'Común',
+                0: '-'
             },
             4:{
                 atributo: 'Número',
                 S: 'Singular',
                 P: 'Plural',
-                N: 'Invariable'
+                N: 'Invariable',
+                0: '-'
             },
             5:{
                 atributo: 'Clasificación semántica',
                 SP: 'Persona',
                 G0: 'Lugar',
                 O0: 'Organización',
-                V0: 'Otros'
+                V0: 'Otros',
+                00: '-'
             },
             7:{
                 atributo: 'Grado',
                 A: 'Aumentativo',
-                D: 'Diminutivo'
+                D: 'Diminutivo',
+                0: '-'
             }
         },
         V:{
             1:{
                 atributo: 'Categoria',
-                V: 'Verbo'
+                V: 'Verbo',
+                0: '-'
             },
             2:{
                 atributo: 'Tipo',
                 M: 'Principal',
                 A: 'Auxiliar',
-                S: 'Semiauxiliar'
+                S: 'Semiauxiliar',
+                0: '-'
             },
             3:{
                 atributo: 'Modo',
@@ -290,7 +377,8 @@ function trans_tag(tag){
                 M: 'Imperativo',
                 N: 'Infinitivo',
                 G: 'Gerundio',
-                P: 'Participio'
+                P: 'Participio',
+                0: '-'
             },
             4:{
                 atributo: 'Tiempo',
@@ -305,23 +393,27 @@ function trans_tag(tag){
                 atributo: 'Persona',
                 1: 'Primera',
                 2: 'Segunda',
-                3: 'Tercera'
+                3: 'Tercera',
+                0: '-'
             },
             6:{
                 atributo: 'Número',
                 S: 'Singular',
-                P: 'Plural'
+                P: 'Plural',
+                0: '-'
             },
             7:{
                 atributo: 'Género',
                 M: 'Masculino',
-                F: 'Femenino'
+                F: 'Femenino',
+                0: '-'
             }
         },
         P:{
             1:{
                 atributo: 'Categoria',
-                P: 'Pronombre'
+                P: 'Pronombre',
+                0: '-'
             },
             2:{
                 atributo: 'Tipo',
@@ -331,115 +423,154 @@ function trans_tag(tag){
                 I: 'Indefinido',
                 T: 'Interrogativo',
                 R: 'Relativo',
-                E: 'Exclamativo'
+                E: 'Exclamativo',
+                0: '-'
             },
             3:{
                 atributo: 'Persona',
                 1: 'Primera',
                 2: 'Segunda',
-                3: 'Tercera'
+                3: 'Tercera',
+                0: '-'
             },
             4:{
                 atributo: 'Género',
                 M: 'Masculino',
                 F: 'Femenino',
                 C: 'Común',
-                N: 'Neutro'
+                N: 'Neutro',
+                0: '-'
             },
             5:{
                 atributo: 'Número',
                 S: 'Singular',
                 P: 'Plural',
-                N: 'Impersonal/Invariable'
+                N: 'Impersonal/Invariable',
+                0: '-'
             },
             6:{
                 atributo: 'Caso',
                 N: 'Nominativo',
                 A: 'Acusativo',
                 D: 'Dativo',
-                O: 'Oblicuo'
+                O: 'Oblicuo',
+                0: '-'
             },
             7:{
                 atributo: 'Poseedor',
                 S: 'Singular',
-                P: 'Plural'
+                P: 'Plural',
+                0: '-'
             },
             8:{
                 atributo: 'Politeness',
                 P: 'Polite',
+                0: '-'
             }
         },
         C:{
             1:{
                 atributo: 'Categoria',
-                C: 'Conjunciones'
+                C: 'Conjunciones',
+                0: '-'
             },
             2:{
                 atributo: 'Tipo',
                 C: 'Coordinada',
-                S: 'Subcoordinada'
+                S: 'Subcoordinada',
+                0: '-'
             }
         },
         I:{
             1:{
                 atributo: 'Categoria',
-                C: 'Interjección'
+                I: 'Interjección',
             }
         },
         S:{
             1:{
                 atributo: 'Categoria',
-                P: 'Preposición'
+                S: 'Preposición',
+                0: '-'
             },
             2:{
                 atributo: 'Tipo',
-                P: 'Preposición'
+                P: 'Preposición',
+                0: '-'
             },
             3:{
                 atributo: 'Forma',
                 S: 'Simple',
-                C: 'Contraída'
+                C: 'Contraída',
+                0: '-'
             },
             4:{
                 atributo: 'Género',
-                M: 'Masculino'
+                M: 'Masculino',
+                0: '-'
             },
             5:{
                 atributo: 'Número',
-                S: 'Singular'
+                S: 'Singular',
+                0: '-'
             }
         },
         F:{
             1:{
                 atributo: 'Categoria',
-                F: 'Signo de puntuación'
+                F: 'Signo de puntuación',
+                0: '-'
             }
         },
         Z:{
             1:{
                 atributo: 'Categoria',
-                Z: 'Numerales'
+                Z: 'Numerales',
+                0: '-'
             },
             2:{
                 atributo: 'Tipo',
                 d: 'Partitivo',
                 p: 'Porcentaje',
-                u: 'Unidad'
+                u: 'Unidad',
+                0: '-'
             }
         },
         W:{
             1:{
                 atributo: 'Categoria',
-                W: 'Fechas y horas'
+                W: 'Fechas y horas',
+                0: '-'
             }
         },
     };
 
-    for(var i=0; i<tag.length; i++) {
-        attr = eagle_tags[tag.charAt(0)][i+1]['atributo'];
-        result.push({attr: eagle_tags[tag.charAt(0)][i+1][tag.charAt(i)]});
-        console.log(result);
+    if(tag.charAt(0) == 'N'){
+        for(var i=0; i<tag.length; i++) {
+            console.log(tag.charAt(i))
+            if(i==4){
+                console.log(tag.charAt(i)+tag.charAt(i+1))
+                var attr = eagle_tags[tag.charAt(0)][i+1]['atributo']
+                result.push({ attr: attr, value: eagle_tags[tag.charAt(0)][i+1][''+tag.charAt(i)+tag.charAt(i+1)]})
+            }else if(i==5){
+                            
+            }else{
+                var attr = eagle_tags[tag.charAt(0)][i+1]['atributo']
+                result.push({ attr: attr, value: eagle_tags[tag.charAt(0)][i+1][tag.charAt(i)]})
+            }
+        }
+    }else{
+        for(var i=0; i<tag.length; i++) {
+            
+            var attr = eagle_tags[tag.charAt(0)][i+1]['atributo']
+            result.push({ attr: attr , value: eagle_tags[tag.charAt(0)][i+1][tag.charAt(i)]})
+        
+        }
     }
+
+    
+    console.log(result)
+    return result
 
 }
 
